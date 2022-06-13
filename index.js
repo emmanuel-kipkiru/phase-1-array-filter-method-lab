912093e8f638504b2a7a5efafc887a1fab2630f1
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-13 22:15:47 +0300

    commit tree 0f63ad209f4e04cdfe4a402c54a754cfb80bc2c1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(driver,driver7){
+    const result = driver.filter(passeddDriver=> passeddDriver.toUpperCase() === driver7.toUpperCase());
+    if(result){
+        return result
+    }
+}
+
+function fuzzyMatch(driver, driver7){
+    const result = driver.filter(passeddDriver=>passeddDriver.toLowerCase().indexof(driver7.toLowerCase())===0)
+    if(result){
+        return result
+    }
+}
+function matchName(driver, driver7){
+    const result = driver.filter(passeddDriver=> passeddDriver.name === driver7);
+    if(result){
+        return result
+    }
+}
